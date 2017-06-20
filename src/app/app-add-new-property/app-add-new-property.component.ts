86e8ee34bc1f05075451ed9506ef8cf92eddbc45
Superman2971 <goldfarb.ian@gmail.com>
2017-06-20 09:01:30 -0700

    added routes and page where we can input new property data to firebase

---
+++ src/app/app-add-new-property/app-add-new-property.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-add-new-property',
+  templateUrl: './app-add-new-property.component.html',
+  styleUrls: ['./app-add-new-property.component.scss']
+})
+export class AppAddNewPropertyComponent implements OnInit {
+
+  window: FirebaseListObservable<any>;
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private db: AngularFireDatabase
+  ) {
+    this.window = db.list('/window-object');
+  }
+
+  ngOnInit() {
+    console.log(this.ActivatedRoute.snapshot.data); // passing in data from router
+  }
+
+  submit(name, type, url, description) {
+    let data = {
+      type: type,
+      example_url: url,
+      description: description
+    };
+    console.log(data);
+    this.window.update(name, data);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { AppWindowObjectComponent } from './app-window-object/app-window-object.component';
+import { AppAddNewPropertyComponent } from './app-add-new-property/app-add-new-property.component';
+
+const appRoutes: Routes = [{
+    path: '',
+    component: AppWindowObjectComponent
+  }, {
+    path: 'add',
+    component: AppAddNewPropertyComponent,
+    data: {
+      object_route: '/window-object'
+    }
+  }, {
+    path: '**', // a catch all for page not found, maybe make a PageNotFoundComponent
+    component: AppWindowObjectComponent
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppWindowObjectComponent
+    AppWindowObjectComponent,
+    AppAddNewPropertyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
     AngularFireDatabaseModule // imports firebase/database, only needed for database features
   ],
